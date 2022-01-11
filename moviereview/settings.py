27f5d54de9f91b18e1643246faad0e995d3a31b8
Harshit Nariya <72091868+harshitnariya8@users.noreply.github.com>
2022-01-11 22:50:24 +0530

    d

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import PIL
+from PIL import Imag
 from pathlib import Path
 import os.path
 import django_heroku
