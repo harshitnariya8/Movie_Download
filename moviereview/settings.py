a8d5fb3c86d1b8b1ac69efe065313f9c789f6577
Harshit Nariya <72091868+harshitnariya8@users.noreply.github.com>
2022-01-15 13:10:06 +0530

    change settings

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-58w2)@y4x1f^=$i-3-!^eyyq#$gf@=qu0#tr31xm#+j)-51_&8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','filmybazar.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','filmybazaaar.herokuapp.com']
 
 
 # Application definition
@@ -137,9 +137,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
 
 SITE_ID = 1
 
