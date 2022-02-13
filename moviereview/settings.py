2fe793704bef5042d9fc618cbde46577f2f3760c
Harshit Nariya <72091868+harshitnariya8@users.noreply.github.com>
2022-02-13 13:56:37 +0530

    Update settings.py

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-58w2)@y4x1f^=$i-3-!^eyyq#$gf@=qu0#tr31xm#+j)-51_&8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','filmybazar.herokuapp.com']
 
