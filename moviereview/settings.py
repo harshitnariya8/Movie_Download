c12356248624b8a5424757478c2ae3af905278fc
Harshit Nariya <72091868+harshitnariya8@users.noreply.github.com>
2022-03-15 12:40:39 +0530

    Update settings.py

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-58w2)@y4x1f^=$i-3-!^eyyq#$gf@=qu0#tr31xm#+j)-51_&8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
